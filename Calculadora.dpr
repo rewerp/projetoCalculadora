d3c24b14145d2a807cc645a916238f74282a7f4d
rewerp <rewer_p@hotmail.com>
2021-09-23 11:42:33 -0300

    Initial commit

---
+++ Calculadora.dpr
@@ -0,0 +1,14 @@
+program Calculadora;
+
+uses
+  Vcl.Forms,
+  uCalculadora in 'uCalculadora.pas' {frmCalculadora};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TfrmCalculadora, frmCalculadora);
+  Application.Run;
+end.
